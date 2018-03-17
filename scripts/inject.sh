93848cd356172f031e1c9a4f8350656dd9082659
knipferrc <knipferrc@gmail.com>
2018-03-16 22:37:59 -0400

    fix inject script

--- scripts/inject.sh
+++ scripts/inject.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd ..
-echo 'require("electron").webFrame.registerURLSchemeAsPrivileged("file")' | cat - $PWD/recode/build/*.js > temp && mv temp $PWD/recode/build/*.js
+echo 'require("electron").webFrame.registerURLSchemeAsPrivileged("file")' | cat - $PWD/termio/build/*.js > temp && mv temp $PWD/termio/build/*.js
